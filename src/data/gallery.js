7583e9c16e1c2905d939391295a87f1635e35335
emirlemes <emir.lemes@gmail.com>
2025-09-15 08:21:02 +0200

    deleted old files

--- src/data/gallery.js
+++
@@ -1,8 +0,0 @@
-export const gallery = [
-  {id:1,src:'/g1.jpg',alt:'Setup 1'},
-  {id:2,src:'/g2.jpg',alt:'Setup 2'},
-  {id:3,src:'/g3.jpg',alt:'Setup 3'},
-  {id:4,src:'/g4.jpg',alt:'Setup 4'},
-  {id:5,src:'/g5.jpg',alt:'Setup 5'},
-  {id:6,src:'/g6.jpg',alt:'Setup 6'},
-];
